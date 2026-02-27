cb0d12f2a651181552535293a61ce24a1b19effc
sa5747-blip <sa5747@srmist.edu.in>
2026-02-27 13:07:43 +0530

    UC1 add welcome message module

--- src/palindromeCheckerApp.java
+++ src/palindromeCheckerApp.java
@@ -1,5 +1,9 @@
-public class palindromeCheckerApp {
-    public static void main(String[]args){
-        System.out.println("welcome to palindrome Checker app");
+public class palindromeCheckerApp{
+    public static void main(String[] args) {
+        System.out.println("===================================");
+        System.out.println("   WELCOME TO PALINDROME APP");
+        System.out.println("===================================");
+        System.out.println("This application checks whether a word is palindrome or not.");
+
     }
 }
