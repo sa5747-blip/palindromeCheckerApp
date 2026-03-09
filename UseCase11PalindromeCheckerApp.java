d53e1fa935300a62d607373120d435eed9defe77
yourgithubusername <youremail@gmail.com>
2026-03-09 23:49:39 +0530

    UC11: Object-oriented palindrome service

---
+++ UseCase11PalindromeCheckerApp.java
@@ -0,0 +1,32 @@
+class PalindromeChecker {
+
+    // Method to check palindrome
+    public boolean checkPalindrome(String word) {
+
+        String reversed = "";
+
+        for(int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        return word.equals(reversed);
+    }
+}
+
+public class UseCase11PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String word = "madam";
+
+        PalindromeChecker checker = new PalindromeChecker();
+
+        boolean result = checker.checkPalindrome(word);
+
+        if(result) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
+    }
+}
