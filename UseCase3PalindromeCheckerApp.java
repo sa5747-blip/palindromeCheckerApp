058df15db245fff14e73abb14dc3b05c8c03b932
yourgithubusername <youremail@gmail.com>
2026-03-09 22:38:54 +0530

    UC3: Palindrome check using string reverse

--- UseCase3PalindromeCheckerApp.java
+++ UseCase3PalindromeCheckerApp.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class UseCase3PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Input string
+        String word = "level";
+
+        // Variable to store reversed string
+        String reverse = "";
+
+        // Reverse the string using loop
+        for(int i = word.length() - 1; i >= 0; i--) {
+            reverse = reverse + word.charAt(i);
+        }
+
+        // Compare original and reversed string
+        if(word.equals(reverse)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
+    }
 }
