505fce73e094b2f93cb03c0f63e97c787fa429b3
yourgithubusername <youremail@gmail.com>
2026-03-09 21:55:49 +0530

    UC1: Application Entry and Welcome Message

---
+++ UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,15 @@
+public class UseCase1PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("================================");
+        System.out.println("   PALINDROME CHECKER APP");
+        System.out.println("================================");
+
+        System.out.println("Application Name : Palindrome Checker");
+        System.out.println("Version : 1.0");
+
+        System.out.println("Welcome to the Palindrome Checker Application!");
+
+    }
+}
